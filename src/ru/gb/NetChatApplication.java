95293c73992e7cdfbdeb13a66299a3f4c0ac74dc
lolbulka <lolbulka@mail.ru>
2022-01-20 20:45:59 +0300

    hm4 done.

--- src/ru/gb/NetChatApplication.java
+++ src/ru/gb/NetChatApplication.java
@@ -1,4 +1,4 @@
-package sample;
+package ru.gb;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
@@ -6,13 +6,13 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
-public class Main extends Application {
+public class NetChatApplication extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 300, 275));
+        Parent root = FXMLLoader.load(getClass().getResource("NetChatApplication.fxml"));
+        primaryStage.setTitle("My Net Chat");
+        primaryStage.setScene(new Scene(root, 500, 500));
         primaryStage.show();
     }
 
